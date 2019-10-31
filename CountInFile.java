16065495e86c02996208ec9600c23c3bde829a07
KrYounger <kryounger@gmail.com>
2019-10-31 11:07:13 -0400

    minor mod before PR

--- CountInFile.java
+++ CountInFile.java
@@ -1,10 +1,6 @@
 public class CountInFile {
 
     public static void main(String[] args) {
-        FileCounting fc = new FileCounting();
-        for (String arg : args) {
-            String r1 = fc.ScanTheFile(arg);
-            System.out.printf("%s %s\n", r1, arg);
-        }
+
     }
 }
